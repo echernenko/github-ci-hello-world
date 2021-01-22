733a85276a237a8448147552db7af8e753dd3998
Eugene Chernenko <chernenko@gmail.com>
2021-01-22 14:49:15 +0100

    adding hello world class for testing

---
+++ Sandbox.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+class Sandbox {
+
+  public static void main(String[] args) {
+    new Sandbox().helper();
+  }
+
+  private String helper() {
+    System.out.println("i am working");
+    return "hello world";
+  }
+
+}
