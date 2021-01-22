b80a6cdf713779970f4b5684b8cc53860bb4a940
Eugene Chernenko <chernenko@gmail.com>
2021-01-22 21:48:12 +0100

    Create Sandbox.java

---
+++ Sandbox.java
@@ -0,0 +1,12 @@
+class Sandbox {
+
+  public static void main(String[] args) {
+    new Sandbox().helper();
+  }
+
+  private String helper() {
+    System.out.println("i am working");
+    return "hello world";
+  }
+
+}
